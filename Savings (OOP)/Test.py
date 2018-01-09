5aa2e20dcb773dfbd9d02790e1866050a0ca3b05
joowweehh <35253719+joowweehh@users.noreply.github.com>
2018-01-09 16:17:12 +0800

    Add files via upload

---
+++ Savings (OOP)/Test.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+import MainProcess
+
+app = Flask(__name__)
+
+@app.route('/savings')
+def savings():
+    savingsList = []
+    savingsList = MainProcess.processSaving()
+    #print the saving list
+    for i in savingsList:
+
+
+    return render_template('savings.html', savings=savingsList, count=len(savingsList))
+
+@app.route('/layout')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/layout1')
+def layout1():
+    return render_template('layout1.html')
+
+if __name__ == '__main__':
+    app.run()
